3b342229db1f6c14c8279bdba7462d1654dd77cd
Michael <myque@live.com>
2018-05-23 22:45:55 -0700

    load custom marker images

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactMapboxGl, {Layer, Feature} from "react-mapbox-gl";
 
+const marker = require("../assets/marker.png");
+const markerImage = new Image(35, 50);
+markerImage.src = marker;
+const images = ["melodyPin", markerImage];
+console.log(markerImage);
+
 const MapRender = ReactMapboxGl({
   accessToken:"pk.eyJ1IjoibXN6aGFuZyIsImEiOiJjamd6cTVmNmoyb24wMzNwOWM4bHBzdngzIn0.0Lij-AdfjrFgJg7dFdSZrA"
 });
@@ -16,6 +22,7 @@ class Map extends React.Component {
       <MapRender
         style="mapbox://styles/mapbox/streets-v8"
         zoom={zoom}
+        images={images}
         containerStyle={{
           height: "100vh",
           width: "100vw"
@@ -23,8 +30,10 @@ class Map extends React.Component {
         <Layer
           type="symbol"
           id="marker"
-          layout={{ "icon-image": "marker-15" }}>
+          layout={{ "icon-image": "melodyPin" }}
+          images={images}>
           <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+          <Feature coordinates={[-0.481747846041145, 50.3233379650232]}/>
         </Layer>
       </MapRender>
     );

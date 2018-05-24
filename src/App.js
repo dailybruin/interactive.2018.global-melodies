f179850b93a36507a22df415e3e5bd04233ff2fe
Michael <myque@live.com>
2018-05-23 17:06:00 -0700

    get rid of uber react-map, use the better react-mapbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Map from "./components/Map";
-
+import "./sass/app.scss";
 class App extends React.Component {
   render() {
     return (
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,28 +1,32 @@
-import React from 'react';
-import ReactMapGL from 'react-map-gl';
+import React from "react";
+import ReactMapboxGl, {Layer, Feature} from "react-mapbox-gl";
+
+const MapRender = ReactMapboxGl({
+  accessToken:"pk.eyJ1IjoibXN6aGFuZyIsImEiOiJjamd6cTVmNmoyb24wMzNwOWM4bHBzdngzIn0.0Lij-AdfjrFgJg7dFdSZrA"
+});
+const zoom = [8];
 
 class Map extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      viewport: {
-        width: 400,
-        height: 400,
-        latitude: 37.7577,
-        longitude: -122.4376,
-        zoom: 8
-      }
-    };
-
   }
 
   render() {
     return (
-      <ReactMapGL
-        {...this.state.viewport}
-        onViewportChange={(viewport) => this.setState({viewport})}
-        mapboxApiAccessToken="pk.eyJ1IjoibXN6aGFuZyIsImEiOiJjamd6cTVmNmoyb24wMzNwOWM4bHBzdngzIn0.0Lij-AdfjrFgJg7dFdSZrA"
-      />
+      <MapRender
+        style="mapbox://styles/mapbox/streets-v8"
+        zoom={zoom}
+        containerStyle={{
+          height: "100vh",
+          width: "100vw"
+      }}>
+        <Layer
+          type="symbol"
+          id="marker"
+          layout={{ "icon-image": "marker-15" }}>
+          <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+        </Layer>
+      </MapRender>
     );
   }
 }
